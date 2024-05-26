6a67228e4e514a47b83bd40d8b27bf9120eee85d
Noah Kaiser <52133373+hfsyntax@users.noreply.github.com>
2024-05-25 23:03:51 -0400

    add url props to component

--- app/components/ProjectCard.jsx
+++ app/components/ProjectCard.jsx
@@ -1,5 +1,5 @@
 import Button from "./Button"
-const ProjectCard = ({imageSrc, title, stack, desc}) => {
+const ProjectCard = ({imageSrc, title, stack, desc, sourceURL, appURL}) => {
     return (
         <div className="project-card">
             <img src={imageSrc} alt='project_picture' />
@@ -7,8 +7,8 @@ const ProjectCard = ({imageSrc, title, stack, desc}) => {
             <span>{desc}</span>
             <span><u>Stack</u></span>
             <span>{stack.join(", ")}</span>
-            <Button>Source Code</Button>
-            <Button>Try It</Button>
+            <Button href={sourceURL} newWindow={true}>Source Code</Button>
+            <Button href={appURL} newWindow={true}>Try It</Button>
         </div>
     )
 }
