8a5154588ddaf5ffd17909bb392a5693ec5cf4e3
Noah Kaiser <52133373+hfsyntax@users.noreply.github.com>
2024-05-25 23:02:17 -0400

    add links to project cards

--- app/page.js
+++ app/page.js
@@ -18,15 +18,36 @@ export default function Portfolio() {
       </span>
       <Button id="learn-more-btn" href='/#about'>Learn More</Button>
       <h1 id='projects' className='section'>Projects</h1>
-      <hr/>
+      <hr />
       <Container className="project-container">
-        <ProjectCard imageSrc="/crossyoff.png" title="CrossyOff" desc="Site for managing player data for the Competitive Crossy Road Community" stack={["Node", "React", "CSS", "MySQL"]} />
-        <ProjectCard imageSrc="/pick6.png" title="Pick6" stack={["HTML", "JavaScript", "CSS", "PHP", "MySQL"]} desc="Site for managing NFL fantasy football selections." />
-        <ProjectCard imageSrc="/biomium.png" title="Biomium" stack={["Node", "JavaScript", "CSS", "Tampermonkey"]} />
+        <ProjectCard
+          imageSrc="/crossyoff.png"
+          title="CrossyOff"
+          desc="Site for managing player data for the Competitive Crossy Road Community"
+          stack={["Node", "React", "CSS", "MySQL"]}
+          sourceURL="https://github.com/hfsyntax/crossyoff"
+          appURL="https://crossyoff.vercel.app/"
+        />
+        <ProjectCard
+          imageSrc="/pick6.png"
+          title="Pick6"
+          desc="Site for managing NFL fantasy football selections."
+          stack={["HTML", "JavaScript", "CSS", "PHP", "MySQL"]}
+          sourceURL="https://github.com/hfsyntax/pick6"
+          appURL="https://pick-6.net/"
+        />
+        <ProjectCard
+          imageSrc="/biomium.png"
+          title="Biomium"
+          desc="Browser extension for the website game biome3d.com"
+          stack={["Node", "JavaScript", "CSS", "Tampermonkey"]}
+          sourceURL="https://github.com/hfsyntax/biomium"
+          appURL="http://biome3d.com/"
+        />
         <ProjectCard imageSrc="/chick.png" title="Chick" desc="Bot for managing the Competitive Crossy Road Community Discord Server" stack={["Node"]} />
       </Container>
       <h1 id='technologies' className='section'>Technologies</h1>
-      <hr/>
+      <hr />
       <span className='description'>
         I have experience with both front-end and back-end technologies.
         However, I prefer working with front-end technologies because I enjoy creating user interfaces.
@@ -35,7 +56,7 @@ export default function Portfolio() {
         <TechCard />
       </Container>
       <h1 id='about' className='section'>About Me</h1>
-      <hr/>
+      <hr />
       <span className='description'>
         I learned about the concept of programming through wanting to make a websites game interface better.
         In the process of creating the interface I realized programming could be used to improve applications
